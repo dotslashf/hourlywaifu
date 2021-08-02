81711db9ec838a447502244466ca831451bd6c68
dotslashf <fadhlurahman.i@gmail.com>
2021-08-02 18:48:12 +0800

    Add sleep

--- src/index.ts
+++ src/index.ts
@@ -2,38 +2,43 @@ import CharacterFinder from './lib/anilist';
 import Formatter from './lib/formatter';
 import Twitter from './lib/twitter';
 import Waifu from './lib/waifu';
+import sleep from './utils/sleep';
 
 (async () => {
   const tw = new Twitter();
 
-  const cf = new CharacterFinder();
-  const char = await cf.getRandomCharacter();
-  const topMedia = await cf.getCharacterMedia(char.id);
+  while (true) {
+    const cf = new CharacterFinder();
+    const char = await cf.getRandomCharacter();
+    const topMedia = await cf.getCharacterMedia(char.id);
 
-  const waifu = new Waifu(char, topMedia);
-  const idMal = await waifu.getIdMal();
-  await waifu.getImgFromMal(idMal);
-  await waifu.downloadAllImage();
+    const waifu = new Waifu(char, topMedia);
+    const idMal = await waifu.getIdMal();
+    await waifu.getImgFromMal(idMal);
+    await waifu.downloadAllImage();
 
-  const f = new Formatter(waifu);
-  const waifuSource = f.waifuSource;
-  const waifuTopMedia = f.topMedia;
+    const f = new Formatter(waifu);
+    const waifuSource = f.waifuSource;
+    const waifuTopMedia = f.topMedia;
 
-  if (waifu.img.others === []) {
-    tw.tweetWaifu(waifuSource).then(async tweet => {
-      await tw.tweetTopMedia(waifuTopMedia, tweet.id_str, waifu.media);
-    });
-  } else {
-    tw.tweetWaifu(waifuSource).then(async tweet => {
-      await tw
-        .tweetImgFromMal(tweet.id_str, waifu.img.others, waifu.name)
-        .then(async secondTweet => {
-          await tw.tweetTopMedia(
-            waifuTopMedia,
-            secondTweet.id_str,
-            waifu.media
-          );
-        });
-    });
+    if (waifu.img.others === []) {
+      tw.tweetWaifu(waifuSource).then(async tweet => {
+        await tw.tweetTopMedia(waifuTopMedia, tweet.id_str, waifu.media);
+      });
+    } else {
+      tw.tweetWaifu(waifuSource).then(async tweet => {
+        await tw
+          .tweetImgFromMal(tweet.id_str, waifu.img.others, waifu.name)
+          .then(async secondTweet => {
+            await tw.tweetTopMedia(
+              waifuTopMedia,
+              secondTweet.id_str,
+              waifu.media
+            );
+          });
+      });
+    }
+
+    await sleep(120);
   }
 })();
---
+++ src/utils/sleep.ts
@@ -0,0 +1,5 @@
+export default function sleep(ms: number): Promise<void> {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms * 1000 * 60);
+  });
+}
